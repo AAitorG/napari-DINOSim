df0da64f4e00dccc1e1ca8da6e3e0ad4bfd71835
Aitor González-Marfil <aitorgacad@gmail.com>
2025-04-11 12:33:54 +0200

    ensure valid preprocessing dtypes

--- src/napari_dinosim/_widget.py
+++ src/napari_dinosim/_widget.py
@@ -772,6 +772,7 @@ class DINOSim_widget(Container):
                     crop_size = self._calculate_crop_size(
                         self.scale_factor_selector.value
                     )
+                    image = self._ensure_valid_dtype(image)
                     self.pipeline_engine.pre_compute_embeddings(
                         image,
                         overlap=(0, 0),
@@ -780,6 +781,11 @@ class DINOSim_widget(Container):
                         verbose=True,
                         batch_size=1,
                     )
+
+    def _ensure_valid_dtype(self, image):
+        """Ensure the image has a valid dtype."""
+        if image.dtype == np.uint16:
+            return image.astype(np.int32)
 
     def _get_nhwc_image(self, image):
         """Convert image to NHWC format (batch, height, width, channels).
