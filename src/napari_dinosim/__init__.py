06ce56d4ddeb36cc09f498fb0fe4d12781048550
Aitor González-Marfil <aitorgacad@gmail.com>
2026-05-30 11:41:43 +0200

    v0.1.5

--- src/napari_dinosim/__init__.py
+++ src/napari_dinosim/__init__.py
@@ -4,7 +4,7 @@ try:
     __version__ = version("napari-dinosim")
 except PackageNotFoundError:
     # package is not installed
-    __version__ = "0.1.4"
+    __version__ = "0.1.5"
 
 from ._widget import DINOSim_widget
 
